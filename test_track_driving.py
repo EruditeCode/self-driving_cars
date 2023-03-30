46f7463ef1cda743cfbc1012314b5c563c0f2982
EruditeCode <erudite.home@gmail.com>
2023-03-30 17:33:37 +0100

    Added basic trail to car visualisation.

--- test_track_driving.py
+++ test_track_driving.py
@@ -3,6 +3,7 @@
 # Video Walkthrough Link: XXXX
 
 import pygame
+from collections import deque
 from test_track_barriers import barriers
 from improved_car import Car
 
@@ -19,14 +20,17 @@ def main():
 	screen = pygame.display.set_mode((WIDTH, HEIGHT))
 	clock = pygame.time.Clock()
 
-	CAR_IMAGE = pygame.image.load('car.png')
-	CAR_IMAGE = pygame.transform.scale(CAR_IMAGE, (42, 20))
-
 	bg = pygame.image.load('test_track.png')
 	bg = pygame.transform.scale(bg, (900, 600))
 
+	# Loading car image and initialising the car object.
+	CAR_IMAGE = pygame.image.load('car.png')
+	CAR_IMAGE = pygame.transform.scale(CAR_IMAGE, (42, 20))
 	car1 = Car((WIDTH//2, HEIGHT//2), 90, 3, 5)
 	car1.update(walls)
+
+	# Initialising empty trail list to create a trail for the car.
+	trail = deque([], 61)
 
 	drive = False
 	while True:
@@ -38,10 +42,21 @@ def main():
 				drive = not drive
 
 		if drive:
+			trail.appendleft(car1.pos)
 			car1.update(walls)
 
 		# Displaying the background surface.
 		screen.blit(bg, (0, 0))
+
+		# Drawing the car trail. # Make the trail smaller over time...
+		if len(trail) < 60:
+			for i in range(0, len(trail)):
+				pygame.draw.circle(screen, (236, 236-i, 236-3*i), trail[i], 4)
+		else:
+			for i in range(0, len(trail)):
+				pygame.draw.circle(screen, (236, 236-i, 236-3*i), trail[i], 4)
+			trail.pop()
+
 
 		# for ray in car1.rays:
 		# 	pygame.draw.aaline(screen, (0,255,0), ray.pos, ray.terminus)
